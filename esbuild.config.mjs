4b98410dd9494c66a9b167579cc5ba425acc401c
星野梦月 <147403913+Hoshino-Yumetsuki@users.noreply.github.com>
2024-11-03 23:23:40 +0800

    更新 esbuild.config.mjs

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -6,14 +6,14 @@ const { copyPlugin } = copyPluginPkg
 const options = {
     entryPoints: ['./src/index.ts'],
     bundle: true,
-    outdir: 'dist',
+    outdir: 'lib',
     minify: true,
     platform: 'node',
     packages: 'external',
     plugins: [
         copyPlugin({
             src: './src/assets',
-            dest: './dist/assets',
+            dest: './lib/assets',
         }),
     ],
 }
